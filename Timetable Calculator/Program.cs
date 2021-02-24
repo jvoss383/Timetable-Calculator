df2ea0be521be7ed168e2e84eb52df1c11a810fa
jvoss383 <42113000+jvoss383@users.noreply.github.com>
2021-02-25 00:13:02 +1300

    Add project files.

---
+++ Timetable Calculator/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Timetable_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getting location data
+            Console.WriteLine("enter path to location lookup table: ");
+            //string locationImportPath = Console.ReadLine().Replace('\"', ' ');
+            string locationImportPath = @"C:\Users\jvoss\Downloads\University Timetable - Locations (3).tsv";
+            Location[] locations = ImportData.Locations(locationImportPath);
+
+            // getting timetable data
+            Console.WriteLine("enter path to timetable data: ");
+            //string timetableDataImportPath = Console.ReadLine().Replace('\"', ' ');
+            string timetableDataImportPath = @"C:\Users\jvoss\Downloads\University Timetable - Timetable Calculator (2).tsv";
+            Event[] events = ImportData.Events(timetableDataImportPath);
+
+            TimetableOption[] timetableOptions = GenerateTimetableOptions(events);
+
+            // generating timetables and calculating their scores
+            Timetable[] timetables = new Timetable[timetableOptions.Count()];
+
+            int validTimetables = 0;
+            for(int i = 0; i < timetables.Count(); i++)
+            {
+                timetables[i] = new Timetable(timetableOptions[i], events); // this line does the heavy lifting of creating the TimeTable object
+                if(timetables[i].valid)
+                {
+                    validTimetables++;
+                    timetables[i].CalcScore(locations);
+                    /*Console.WriteLine(String.Format(
+                        "valid timetables: {0}\n" +
+                        "current timetable index: {1}\n" +
+                        "total timetables: {2}\n" +
+                        "current timetable score: {3}",
+                        validTimetables,
+                        i, 
+                        timetables.Count(),
+                        timetables[i].score));//*/
+                }
+            }
+
+            // filtering out invalid timetables
+            timetables = Timetable.FilterValidTimetables(timetables);
+
+            Console.WriteLine("sorting...");
+            Timetable.SortTimetables(timetables, Timetable.SortOrder.ascending);
+            Console.WriteLine("done");
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("enter timetable index: ");
+                    timetables[Convert.ToInt32(Console.ReadLine())].PrintTimetable();
+                }
+                catch
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+        }
+
+        static TimetableOption[] GenerateTimetableOptions(Event[] events)
+        {
+            int combinations = 1;
+            foreach(Event event_ in events)
+            {
+                combinations *= event_.options.Count();
+            }
+
+            TimetableOption[] timetableOptions = new TimetableOption[combinations];
+            // setting first timetableOption to all zeroes
+            timetableOptions[0] = new TimetableOption(new int[events.Count()]);
+            for (int i = 0; i < timetableOptions[0].eventOptionIndices.Count(); i++)
+            {
+                timetableOptions[0].eventOptionIndices[i] = 0;
+            }
+
+            // generating all other possible combinations
+            for (int i = 1; i < combinations; i++)
+            {
+                timetableOptions[i] = IterateTimetableOptionIndices(timetableOptions[i - 1], events);
+            }
+
+            return timetableOptions;
+        }
+
+        /// <summary>
+        /// Basically a counter where the base of each digit is equal to the number of eventOptions available for that event
+        /// </summary>
+        private static TimetableOption IterateTimetableOptionIndices(TimetableOption timetableOptionIndices, Event[] events)
+        {
+            TimetableOption newTimetableOptionIndices = new TimetableOption((int[])timetableOptionIndices.eventOptionIndices.Clone());
+            for (int eventIndex = newTimetableOptionIndices.eventOptionIndices.Count() - 1; eventIndex > 0; eventIndex--) // should be while eventIndex > 0 so that it never directly iterates the most significant digit, otherwise it will eventually try to rollover into the [-1]th digit
+            {
+                newTimetableOptionIndices.eventOptionIndices[eventIndex]++;
+
+                // then this pass rolls it over to 0, and the next index increases by one
+                if (newTimetableOptionIndices.eventOptionIndices[eventIndex] == events[eventIndex].options.Count())
+                {
+                    newTimetableOptionIndices.eventOptionIndices[eventIndex] = 0;
+                    //newTimetableOptionIndices.eventOptionIndices[eventIndex - 1]++; // no need to iterate because it will hit the normal iteration line next anyway
+                }
+                else // just iterating the current eventIndex was fine, no further rollovers required
+                {
+                    return newTimetableOptionIndices;
+                }
+            }
+            return null;
+        }
+    }
+
+
+    public class ImportData
+    {
+        public static Location[] Locations(string importPath)
+        {
+            string[] file = File.ReadAllLines(importPath);
+            Location[] locations = new Location[file.Count() - 1];
+            for(int i = 1; i < file.Count(); i++)
+            {
+                string[] splitLine = file[i].Split('\t'); // for .tsv files
+                locations[i - 1] = new Location(splitLine[0], Convert.ToDouble(splitLine[1]), Convert.ToDouble(splitLine[2]), Convert.ToDouble(splitLine[3]));
+            }
+            return locations;
+        }
+
+        public static Event[] Events(string importPath)
+        {
+            string[] file = File.ReadAllLines(importPath);
+
+            string currentPaper = "";
+            List<Event> events = new List<Event>();
+            for(int row = 0; row < file.Count(); row++)
+            {
+                string[] splitRow = file[row].Split('\t');
+
+                // if row is labelled "Paper"
+                if(splitRow[0] == "Paper")
+                {
+                    currentPaper = splitRow[1];
+                    continue;
+                }
+
+                // if row is labelled "Event"
+                if(splitRow[0] == "Event")
+                {
+                    // go horizontally across the events
+                    for (int column = 1; column < splitRow.Count(); column++)
+                    {
+                        // go vertically adding the event options
+                        Event newEvent = new Event(currentPaper, splitRow[column]);
+                        
+                        for(int eventOptionIndex = 1; row + eventOptionIndex < file.Count(); eventOptionIndex++)
+                        {
+                            // if row is empty then there are no more EventOptions for this event
+                            if (file[row + eventOptionIndex].Split('\t')[column] == "")
+                            {
+                                break;
+                            }
+
+                            newEvent.options.Add(new EventOption(currentPaper, file[row + eventOptionIndex].Split('\t')[column]));
+                        }
+                        events.Add(newEvent);
+                    }
+                }
+            }
+            return events.ToArray();
+        }
+    }
+
+    public class DistanceCalc
+    {
+        public static double Distance(Location pointA, Location pointB)
+        {
+            double radius = 6371;
+            double dLatitude = ToRadians(pointA.latitude - pointB.latitude);
+            double dLongitude = ToRadians(pointA.longitude - pointB.longitude);
+
+            double a =
+              Math.Sin(dLatitude / 2) * Math.Sin(dLatitude / 2) +
+              Math.Cos(ToRadians(pointA.latitude)) * Math.Cos(ToRadians(pointB.latitude)) *
+              Math.Sin(dLongitude / 2) * Math.Sin(dLongitude / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = radius * c;
+
+            double correctionFactor = 23890.17d / 31150.28d;
+            d *= correctionFactor;
+            //d *= 1000; // km -> m
+            return d;
+        }
+
+        private static double ToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+    }
+
+    public class Location
+    {
+        public string name;
+        public double longitude;
+        public double latitude;
+        public double altitude;
+
+        public Location(string name_In, double longitude_In, double latitude_In, double altitude_In)
+        {
+            name = name_In;
+            longitude = longitude_In;
+            latitude = latitude_In;
+            altitude = altitude_In;
+        }
+    
+        public static Location ToLocation(string locationName, Location[] locations)
+        {
+            foreach(Location location in locations)
+            {
+                if(location.name.ToLower() == locationName.ToLower())
+                {
+                    return location;
+                }
+            }
+            return null;
+        }
+    }
+}
+
+namespace ExtensionMethods
+{ 
+    public static class Extensions
+    {
+        public static Timetable_Calculator.Event FindEvent (this List<Timetable_Calculator.Event> list, string name)
+        {
+            foreach (Timetable_Calculator.Event timeTableEvent in list)
+            {
+                if(timeTableEvent.name == name)
+                {
+                    return timeTableEvent;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Timetable Calculator/Timetable.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable_Calculator
+{
+    public class Timetable
+    {
+        public bool valid = false;
+        public double score;
+        public Day[] days = new Day[7];
+
+        public enum SortOrder
+        {
+            ascending,
+            descending
+        }
+
+        public string PrintTimetable()
+        {
+            int headerLines = 2;
+            int footerLines = 2;
+            int columnSize = 22;
+            int hoursColumnWidth = 9;
+            int rowSize = 5;
+            int startHour = 8;
+            int endHour = 17;
+            int startDay = 1;
+            int endDay = 5;
+            string[] daysOfWeek = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+            string[] output = new string[(endHour - startHour) * rowSize + headerLines + footerLines];
+
+            // hours column
+            // padding header lines
+            for(int row = 0; row < headerLines; row++)
+            {
+                output[row] += NormLen("", hoursColumnWidth - 1, ' ') + "|";
+            }
+            //padding footer rows
+            for(int row = output.Count() - 1; row > output.Count() - 1 - footerLines; row--)
+            {
+                output[row] += NormLen("", hoursColumnWidth - 1, ' ') + "|";
+            }
+            // main hours column loop
+            for (int hour = 0; hour < endHour - startHour; hour++)
+            {
+                // print 24hr time
+                output[headerLines + hour * rowSize] += NormLen(hour + startHour + ":00", hoursColumnWidth - 1, ' ') + '|';
+                // print 12hr time
+                if(hour + startHour > 12)
+                {
+                    output[headerLines + hour * rowSize + 1] += NormLen((hour - 12) + startHour + ":00", hoursColumnWidth - 1, ' ') + '|';
+                }
+                else
+                {
+                    output[headerLines + hour * rowSize + 1] += NormLen("", hoursColumnWidth - 1, ' ') + "|";
+                }
+
+                // pad remaining rows
+                for(int rowOffset = 2; rowOffset < rowSize; rowOffset++)
+                {
+                    output[headerLines + hour * rowSize + rowOffset] += NormLen("", hoursColumnWidth - 1, ' ') + "|";
+                }
+            }
+
+
+            for (int day = startDay; day < endDay; day++)
+            {
+                // header
+                output[0] += NormLen(daysOfWeek[day], columnSize - 1, ' ') + "|";
+                output[1] += NormLen(""             , columnSize - 1, ' ') + "|";
+
+                // footer
+                output[output.Count() - 2] += NormLen("", columnSize - 1, ' ') + "|"; ;
+                output[output.Count() - 1] += NormLen(Convert.ToString(days[day].score), columnSize - 1, ' ') + "|"; ;
+
+                for (int hour = startHour; hour < endHour; hour++)
+                {
+                    // pad if the slot is empty / shouldn't be printed
+                    if (days[day].hours[hour] == null ||
+                        days[day].hours[hour].paper == "Study Time" ||
+                        days[day].hours[hour].paper == "Transport")
+                    {
+                        for (int rowOffset = 0; rowOffset < rowSize; rowOffset++)
+                        {
+                            output[headerLines + (hour - startHour) * rowSize + rowOffset] += NormLen("", columnSize - 1, rowOffset == rowSize - 1 ? '-' : ' ') + "|";
+                        }
+                    }
+                    else
+                    {
+                        output[headerLines + (hour - startHour) * rowSize    ] += NormLen(days[day].hours[hour].paper,        columnSize - 1, ' ') + "|";
+                        output[headerLines + (hour - startHour) * rowSize + 1] += NormLen("",                                 columnSize - 1, ' ') + "|"; // human readable paper name
+                        output[headerLines + (hour - startHour) * rowSize + 2] += NormLen(days[day].hours[hour].name,         columnSize - 1, ' ') + "|";
+                        output[headerLines + (hour - startHour) * rowSize + 3] += NormLen(days[day].hours[hour].fullLocation, columnSize - 1, ' ') + "|";
+                        output[headerLines + (hour - startHour) * rowSize + 4] += NormLen("",                                 columnSize - 1, '-') + "|";
+                    }
+                }
+            }
+
+            string singleOutput = ""; // in one index with newlines rather than in an array
+            foreach(string line in output)
+            {
+                singleOutput += line + "\n";
+            }
+
+            Console.WriteLine(singleOutput);
+            return singleOutput;
+        }
+
+        /// <summary>
+        ///  Normalize length of string by adding leading and trailing space chars
+        /// </summary>
+        private string NormLen(string input, int length, char repeatedChar)
+        {
+            if(input.Length < length)
+                return
+                    new String(repeatedChar, (int)Math.Floor((length - input.Length) / 2d)) +
+                    input +
+                    new String(repeatedChar, (int)Math.Ceiling((length - input.Length) / 2d));
+            else
+            {
+                return input.Substring(0, length);
+            }
+        }
+
+        public static Timetable[] FilterValidTimetables(Timetable[] timetables)
+        {
+            List<Timetable> validTimetables = new List<Timetable>();
+            foreach(Timetable timetable in timetables)
+            {
+                if(timetable.valid)
+                {
+                    validTimetables.Add(timetable);
+                }
+            }
+            return validTimetables.ToArray();
+        }
+
+        public static Timetable[] SortTimetables(Timetable[] timetables, SortOrder sortOrder)
+        {
+            float divisor = 1.3f;
+            int gap = (int)(timetables.Count() / divisor);
+            bool sorted = true;
+            do
+            {
+                sorted = true;
+                gap = (int)(gap / divisor);
+                if (gap < 1)
+                    gap = 1;
+
+                for(int i = 0; i + gap < timetables.Count(); i++)
+                {
+                    if(sortOrder == SortOrder.ascending)
+                    {   // if out of order
+                        if(timetables[i].score > timetables[i + gap].score)
+                        {
+                            sorted = false;
+                            Timetable temp = timetables[i];
+                            timetables[i] = timetables[i + gap];
+                            timetables[i + gap] = temp;
+                        }
+                    }
+                    else // descending
+                    {
+                        if (timetables[i].score < timetables[i + gap].score)
+                        {   // if out of order
+                            sorted = false;
+                            Timetable temp = timetables[i];
+                            timetables[i] = timetables[i + gap];
+                            timetables[i + gap] = temp;
+                        }
+                    }
+                }
+
+                // double check that it's sorted
+                if (sorted)
+                {
+                    sorted = Sorted(timetables, sortOrder);
+                }
+            } while (!sorted);
+
+            return timetables;
+        }
+
+        private static bool Sorted(Timetable[] timetables, SortOrder sortOrder)
+        {
+            for(int i = 0; i < timetables.Count() - 1; i++)
+            {
+                if(sortOrder == SortOrder.ascending)
+                {
+                    if (timetables[i].score > timetables[i + 1].score)
+                        return false;
+                }
+                else
+                {   // is descending
+                    if (timetables[i].score < timetables[i + 1].score)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public Timetable(TimetableOption timetableOption, Event[] events)
+        {
+            // calling days constructors
+            for(int day = 0; day < days.Count(); day++)
+            {
+                days[day] = new Day();
+            }
+
+            // putting all predefined events into timetable
+            for(int eventIndex = 0; eventIndex < events.Count(); eventIndex++)
+            {
+                EventOption eventOption = events[eventIndex].options[timetableOption.eventOptionIndices[eventIndex]];
+                for(int time = eventOption.startTime; time < eventOption.endTime; time++)
+                {
+                    if(days[(int)eventOption.day].hours[time] != null)
+                    {   // then an event is already on at this time, threfore this is a clash and the timetableOption is invalid
+                        valid = false;
+                        return;
+                    }
+                    else
+                    {   // timeslot is available
+                        days[(int)eventOption.day].hours[time] = eventOption;
+                    }
+                }
+            }
+
+            EventOption BikeRack = new EventOption("Transport", ",,I Bike Rack.0.0,Bike Rack");
+                                                              //",,LSL.1.02 , Lab A"
+            EventOption Library = new EventOption("Study Time", ",,M.3.0,Library");
+
+            // entering M block as home location for gaps in timetable, entering I block bike rack as start and end caps
+            for(int day = 0; day < days.Count(); day++)
+            {
+                // finding start time, setting bike rack start cap
+                for(int hour = 0; hour < days[day].hours.Count(); hour++)
+                {
+                    if(days[day].hours[hour] != null)
+                    {   // first filled slot
+                        days[day].startTime = hour;
+                        if(hour >= 1)
+                        {
+                            days[day].hours[hour - 1] = BikeRack;
+                        }
+                        break;
+                    }
+                }
+
+                // finding end time, setting bike rack end cap
+                for(int hour = days[day].hours.Count() - 1; hour >= 0; hour--)
+                {
+                    if (days[day].hours[hour] != null)
+                    {   // last filled slot
+                        days[day].endTime = hour;
+                        if (hour < 23)
+                        {
+                            days[day].hours[hour + 1] = BikeRack;
+                        }
+                        break;
+                    }
+                }
+
+                // filling all empty slots with the library
+                for(int hour = days[day].startTime; hour < days[day].endTime; hour++)
+                {
+                    if(days[day].hours[hour] == null)
+                    {
+                        days[day].hours[hour] = Library;
+                    }
+                }
+            }
+            valid = true; // says that this timetable encountered no clashes while being constructed and could be used
+        }
+
+        public double CalcScore(Location[] locations)
+        {
+            const double altitudePenalty = 20; // adjust this depending on how much you hate hills. Higher value = more hate
+
+            for (int day = 0; day < days.Count(); day++)
+            {
+                for (int hour = days[day].startTime; hour < days[day].endTime - 1; hour++) // -1 end stop because it compares the current index with index+1
+                {
+                    Location locationA = Location.ToLocation(days[day].hours[hour].block, locations);
+                    Location locationB = Location.ToLocation(days[day].hours[hour + 1].block, locations);
+                    days[day].score += DistanceCalc.Distance(locationA, locationB);
+
+                    // elevation change penalty calculation
+                    double deltaElevation = Math.Abs(locationB.altitude - locationA.altitude);
+                    if (deltaElevation > 0)
+                    {   // uphill
+                        days[day].score += deltaElevation * altitudePenalty;
+                    }
+                    else
+                    {   // downhill
+                        days[day].score += deltaElevation * altitudePenalty / 2;
+                    }
+                }
+                score += days[day].score;
+                if(days[day].score < 0)
+                {
+                    throw new Exception("semantically incorrect result");
+                }
+            }
+            return score;
+        }
+    }
+
+    public class Day
+    {
+        public int startTime, endTime;
+        public double score;
+        public EventOption[] hours = new EventOption[24];
+    }
+
+    public class TimetableOption
+    {
+        public int[] eventOptionIndices;
+
+        public TimetableOption(int[] eventOptionIndices)
+        {
+            this.eventOptionIndices = eventOptionIndices;
+        }
+    }
+
+    public class Event
+    {
+        public string paper;
+        public string name;
+        public List<EventOption> options;
+
+        public Event(string paper, string name, List<EventOption> options)
+        {
+            this.paper = paper;
+            this.name = name;
+            this.options = options;
+        }
+        
+        public Event(string paper, string name)
+        {
+            this.paper = paper;
+            this.name = name;
+            this.options = new List<EventOption>();
+        }
+    }
+
+    public class EventOption
+    {
+        public string paper;
+        public string name;
+
+        public int startTime; // times are done in hours, i.e. 11:00am = 11, 3:00pm = 15, etc.. 
+        public int endTime;
+        public int duration;
+
+        public string fullLocation;
+        public string block;
+        public int floor;
+        public int room;
+        public DayEnum day;
+
+        public enum DayEnum
+        {
+            Sunday = 0,
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+
+            Invalid = -1
+        }
+
+        public EventOption(string paper, string input)
+        {
+            string[] splitInput = input.Split(',');
+            this.paper = paper;
+
+            day = ToDay(splitInput[0]);
+
+            if(splitInput[1].Contains('-'))
+            {
+                startTime = Convert.ToInt32(splitInput[1].Split('-')[0].Split(':')[0]);
+                endTime = Convert.ToInt32(splitInput[1].Split('-')[1].Split(':')[0]);
+                duration = endTime - startTime;
+            }
+
+            fullLocation = splitInput[2].Replace(" ", "");
+            block = fullLocation.Split('.')[0];
+            string tempFloor = fullLocation.Split('.')[1].Replace(" ", "");
+            if(tempFloor == "G")
+                floor = 0;
+            else if(tempFloor == "B")
+                floor = -1;
+            else
+                floor = Convert.ToInt32(tempFloor);
+            room = Convert.ToInt32(fullLocation.Split('.')[2]);
+
+            name = splitInput[3];
+            if(name[0] == ' ')
+            {
+                name = name.Substring(1, name.Length - 1);
+            }
+        }
+
+        static DayEnum ToDay(string input)
+        {
+            if (input.Length < 3)
+                return DayEnum.Invalid;
+            switch(input.Substring(0, 3).ToLower())
+            {
+                case "sun":
+                    return DayEnum.Sunday;
+                case "mon":
+                    return DayEnum.Monday;
+                case "tue":
+                    return DayEnum.Tuesday;
+                case "wed":
+                    return DayEnum.Wednesday;
+                case "thu":
+                    return DayEnum.Thursday;
+                case "fri":
+                    return DayEnum.Friday;
+                case "sat":
+                    return DayEnum.Saturday;
+            }
+            return DayEnum.Invalid;
+        }
+    }
+}
